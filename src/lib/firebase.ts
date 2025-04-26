b45bf1e09933662a0ab65a9af6f01330bcfb372b
parshav92 <dedhiaparshav@gmail.com>
2025-04-26 15:44:25 +0530

    use env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUhZ5qy_yKp1_0Mcym53_yTyKsNOIevJ8",
-  authDomain: "todo-fa18b.firebaseapp.com",
-  projectId: "todo-fa18b",
-  storageBucket: "todo-fa18b.firebasestorage.app",
-  messagingSenderId: "286835601807",
-  appId: "1:286835601807:web:c9df8ba40c1bc842ee1bfa",
-  measurementId: "G-TKGRERKFQG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
